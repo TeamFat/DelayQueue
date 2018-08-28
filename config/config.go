dc0f5b4807612aa3a7ca76a51a419a51155e88a2
jay chen <chenjiesuper@gmail.com>
2018-08-28 13:22:26 +0800

    first add

---
+++ config/config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"os"
+
+	"github.com/astaxie/beego/logs"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Name string
+}
+
+func Init(cfg string) error {
+	c := Config{
+		Name: cfg,
+	}
+
+	// 初始化配置文件
+	if err := c.initConfig(); err != nil {
+		return err
+	}
+
+	// 初始化日志包
+	c.initLog()
+
+	return nil
+}
+
+func (c *Config) initConfig() error {
+	if c.Name != "" {
+		viper.SetConfigFile(c.Name) // 如果指定了配置文件，则解析指定的配置文件
+	} else {
+		viper.AddConfigPath("conf") // 如果没有指定配置文件，则解析默认的配置文件
+		viper.SetConfigName("config")
+	}
+	viper.SetConfigType("yaml")                  // 设置配置文件格式为YAML
+	if err := viper.ReadInConfig(); err != nil { // viper解析配置文件
+		return err
+	}
+
+	return nil
+}
+
+func (c *Config) initLog() {
+	os.MkdirAll("./log/", 0777)
+	logs.EnableFuncCallDepth(true)
+	logs.SetLogFuncCallDepth(3)
+	filename := "./log/returncode.log"
+	logs.SetLogger("file", `{"filename":"`+filename+`","maxdays":7}`)
+	logs.SetLevel(logs.LevelDebug)
+}
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"net/http"
+	"time"
+
+	"github.com/TeamFat/DelayQueue/config"
+	"github.com/TeamFat/DelayQueue/router"
+
+	"github.com/astaxie/beego/logs"
+	"github.com/gin-gonic/gin"
+
+	"github.com/spf13/viper"
+)
+
+var (
+	cfg = flag.String("config", "", "apiserver config file path.")
+)
+
+func main() {
+	flag.Parse()
+
+	// init config
+	if err := config.Init(*cfg); err != nil {
+		panic(err)
+	}
+
+	// Set gin mode.
+	gin.SetMode(viper.GetString("runmode"))
+
+	// Create the Gin engine.
+	g := gin.New()
+
+	middlewares := []gin.HandlerFunc{}
+
+	// Routes.
+	router.Load(
+		// Cores.
+		g,
+
+		// Middlwares.
+		middlewares...,
+	)
+
+	// Ping the server to make sure the router is working.
+	go func() {
+		if err := pingServer(); err != nil {
+			logs.Emergency("The router has no response, or it might took too long to start up.", err)
+		}
+		logs.Info("The router has been deployed successfully.")
+	}()
+
+	logs.Info("Start to listening the incoming requests on http address:", viper.GetString("addr"))
+	logs.Info(http.ListenAndServe(viper.GetString("addr"), g).Error())
+}
+
+// pingServer pings the http server to make sure the router is working.
+func pingServer() error {
+	for i := 0; i < viper.GetInt("max_ping_count"); i++ {
+		// Ping the server by sending a GET request to `/health`.
+		resp, err := http.Get(viper.GetString("url") + "/sd/health")
+		if err == nil && resp.StatusCode == 200 {
+			return nil
+		}
+
+		// Sleep for a second to continue the next ping.
+		logs.Info("Waiting for the router, retry in 1 second.")
+		time.Sleep(time.Second)
+	}
+	return errors.New("Cannot connect to the router.")
+}
